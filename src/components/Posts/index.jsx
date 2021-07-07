7fbd11df877372e06f72456363aa1b78f050deed
Mateus Farias <mateus.f.silva@outlook.com>
2021-07-07 00:25:29 -0300

    Initial

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+import { PostCard } from '../PostCard';
+
+export const Posts = ({ posts }) => (
+  <div className="posts">
+            {posts.map(post => (
+              <PostCard
+                key={post.id}
+                post={post}
+              />
+            ))}
+        </div>
+)
